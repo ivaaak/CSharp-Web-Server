414caef6e90ce73dbf5dea82ad2868a96597a8af
ivaaak <43663336+ivaaak@users.noreply.github.com>
2022-03-25 17:45:52 +0200

    basic server initial upload

---
+++ BasicWebServer/BasicWebServer.Server/Controllers/Controller.cs
@@ -0,0 +1,52 @@
+﻿using BasicWebServer.Server.HTTP;
+using BasicWebServer.Server.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicWebServer.Server.Controllers
+{
+    public class Controller
+    {
+        protected Request Request { get; private init; }
+
+        public Controller(Request request)
+        {
+            Request = request;
+        }
+
+        protected Response Text(string text) => new TextResponse(text);
+        protected Response Html(string text) => new HtmlResponse(text);
+        protected Response Html(string html, CookieCollection cookies)
+        { 
+            var response = new HtmlResponse(html);
+
+            if (cookies != null)
+            {
+                foreach (var cookie in cookies)
+                {
+                    response.Cookies.Add(cookie.Name, cookie.Value);
+                }
+            }
+
+            return response;
+        }
+
+        protected Response BadRequest() => new BadRequestResponse();
+        protected Response Unauthorized() => new UnauthorizedResponse();
+        protected Response NotFound() => new NotFoundResponse();
+        protected Response Redirect(string location) => new RedirectResponse(location);
+        protected Response File(string fileName) => new FileResponse(fileName);
+        protected Response View([CallerMemberName] string viewName = "")
+            => new ViewResponse(viewName, GetControllerName());
+        protected Response View(object model, [CallerMemberName] string viewName = "")
+            => new ViewResponse(viewName, GetControllerName(), model);
+
+        private string GetControllerName() 
+            => this.GetType().Name
+                .Replace(nameof(Controller), string.Empty);
+    }
+}
---
+++ BasicWebServer/BasicWebServer.Server/Responses/ViewResponse.cs
@@ -0,0 +1,57 @@
+﻿using BasicWebServer.Server.HTTP;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicWebServer.Server.Responses
+{
+    public  class ViewResponse : ContentResponse
+    {
+        private const char PathSeparator = '/';
+
+        public ViewResponse(string viewName, string controllerName, object model = null)
+            : base(string.Empty, ContentType.Html)
+        {
+            if (!viewName.Contains(PathSeparator))
+            {
+                viewName = controllerName + PathSeparator + viewName;
+            }
+
+            var viewPath = Path
+                .GetFullPath($"./Views/{viewName.TrimStart(PathSeparator)}.cshtml");
+            var viewContent = File.ReadAllText(viewPath);
+
+            if (model != null)
+            {
+                viewContent = PopulateModel(viewContent, model);
+            }
+
+            Body = viewContent;
+        }
+
+        private string PopulateModel(string viewContent, object model)
+        {
+            var data = model
+                .GetType()
+                .GetProperties()
+                .Select(p => new
+                {
+                    p.Name,
+                    Value = p.GetValue(model)
+                });
+
+            foreach (var item in data)
+            {
+                const string openingBrackets = "{{";
+                const string closingBrackets = "}}";
+
+                viewContent = viewContent.Replace($"{openingBrackets}{item.Name}{closingBrackets}", item.Value.ToString());
+            }
+
+            return viewContent;
+        }
+    }
+}
