15f295a9fc1f39cf6f69ad7950b80659441d2647
ivaaak <43663336+ivaaak@users.noreply.github.com>
2022-04-06 11:03:05 +0300

    Update IData.cs

--- Demo/Data/IData.cs
+++ Demo/Data/IData.cs
@@ -1,8 +1,8 @@
-﻿namespace MyWebServer.Demo.Data
-{
-    using System.Collections.Generic;
-    using MyWebServer.Demo.Data.Models;
+using System.Collections.Generic;
+using MyWebServer.Demo.Data.Models;
 
+namespace MyWebServer.Demo.Data
+{
     public interface IData
     {
         IEnumerable<Cat> Cats { get; }
