b33b1b53a5ef10e36f4f6dc7460a8ccea4036d2f
ivaaak <43663336+ivaaak@users.noreply.github.com>
2022-04-18 10:03:28 +0300

    git project - initial commit

---
+++ Git/Data/Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using static DataConstants;
+    
+namespace Git.Data.Models
+{
+    public class User
+    {
+        [Key]
+        [Required]
+        [MaxLength(IdMaxLength)]
+        public string Id { get; init; } = Guid.NewGuid().ToString();
+
+        [Required]
+        [MaxLength(DefaultMaxLength)]
+        public string Username { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        public IEnumerable<Repository> Repositories { get; init; } = new List<Repository>();
+
+        public IEnumerable<Commit> Commits { get; init; } = new List<Commit>();
+    }
+}
---
+++ Git/Services/IValidator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Git.Models.Repositories;
+using Git.Models.Users;
+    
+namespace Git.Services
+{
+    public interface IValidator
+    {
+        ICollection<string> ValidateUser(RegisterUserFormModel model);
+
+        ICollection<string> ValidateRepository(CreateRepositoryFormModel model);
+    }
+}
