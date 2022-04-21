c0017297ea7eeb2674eb12db3ff27a7a1b9d5c3e
ivaaak <43663336+ivaaak@users.noreply.github.com>
2022-04-21 16:05:14 +0300

    initial upload

---
+++ CarShop/Data/CarShopDbContext.cs
@@ -0,0 +1,22 @@
+﻿namespace CarShop.Data
+{
+    using CarShop.Data.Models;
+    using Microsoft.EntityFrameworkCore;
+
+    public class CarShopDbContext : DbContext
+    {
+        public DbSet<User> Users { get; init; }
+
+        public DbSet<Car> Cars { get; init; }
+
+        public DbSet<Issue> Issues { get; init; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=DESKTOP-6PR2R6Q\\SQLEXPRESS01;Database=CarShop;Trusted_Connection=True");
+            }
+        }
+    }
+}
